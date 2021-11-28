50f4405959e54ee2515a0bbaee2ecefc19ab3069
JesusTafolla <chuy_tafolla@hotmail.com>
2021-11-28 21:33:32 +0000

    Change IP

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import {APOLLO_OPTIONS} from 'apollo-angular';
 import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 
-const uri = 'http://34.125.10.64:8080/graphql/'; // <-- add the URL of the GraphQL server here
+const uri = 'http://34.125.253.132:8080/graphql/'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
